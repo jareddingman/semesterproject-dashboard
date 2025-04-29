5a8bcaab9d1be13bd132d23c3e744eb910a19881
jareddingman <jareddingman@gmail.com>
2025-04-29 14:31:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,14 @@ st.set_page_config(page_title='Semester Project')
 # Declare some useful functions.
 
 @st.cache_data
+def load_original_data():
+    url = 'https://raw.githubusercontent.com/jareddingman/semesterproject-dashboard/main/data/UNO%20Service%20Learning%20Data%20Sheet%20De-Identified%20Version(1).xlsx'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return pd.read_excel(url, engine = "openpyxl")
+    else:
+        st.error("Failed to load data from GitHub.")
+        return None
 def get_gdp_data():
     """Grab GDP data from a CSV file.
 
