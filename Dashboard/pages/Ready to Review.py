c39fd1c46bd658c05be25d17d7552cdf766953ff
jareddingman <jareddingman@gmail.com>
2025-05-04 18:48:32 -0500

    Update Ready to Review.py

--- Dashboard/pages/Ready to Review.py
+++ Dashboard/pages/Ready to Review.py
@@ -10,19 +10,28 @@ import numpy as np
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(page_title='Semester Project')
-
+uploadedFile = st.file_uploader("Choose file here:", type = ["csv", "xlsx"])
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
 
 @st.cache_data
 def load_original_data():
-    url = 'https://raw.githubusercontent.com/jareddingman/semesterproject-dashboard/main/data/UNO%20Service%20Learning%20Data%20Sheet%20De-Identified%20Version(1).xlsx'
-    response = requests.get(url)
-    if response.status_code == 200:
-        return pd.read_excel(url, engine = "openpyxl")
-    else:
-        st.error("Failed to load data from GitHub.")
-        return None
+    if uploadedFile == None:
+        url = 'https://raw.githubusercontent.com/jareddingman/semesterproject-dashboard/main/data/UNO%20Service%20Learning%20Data%20Sheet%20De-Identified%20Version(1).xlsx'
+        response = requests.get(url)
+        if response.status_code == 200:
+            return pd.read_excel(url, engine = "openpyxl")
+        else:
+            st.error("Failed to load data from GitHub.")
+            return None
+# def load_original_data():
+#     url = 'https://raw.githubusercontent.com/jareddingman/semesterproject-dashboard/main/data/UNO%20Service%20Learning%20Data%20Sheet%20De-Identified%20Version(1).xlsx'
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         return pd.read_excel(url, engine = "openpyxl")
+#     else:
+#         st.error("Failed to load data from GitHub.")
+#         return None
 #-----------------------------------------------------------------------------
 df_initial = load_original_data()
 
