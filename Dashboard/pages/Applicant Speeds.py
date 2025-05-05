635dd019fc4732ab8f61a501e05123ca9c268250
jareddingman <jareddingman@gmail.com>
2025-05-04 20:13:47 -0500

    Update Applicant Speeds.py

--- Dashboard/pages/Applicant Speeds.py
+++ Dashboard/pages/Applicant Speeds.py
@@ -45,8 +45,7 @@ df = df.dropna(subset = ['Grant Req Date', 'Payment Submitted?'])
 df['Response Time'] = (df['Payment Submitted?'] - df['Grant Req Date']).dt.days
 
 st.subheader("Histogram of Application Speed")
-histogram = px.histogram(df, x = 'Response Time', nbins = 20, labels = {
-    'Response time': 'Response time (in days)'}, title = "Application Speed")
+histogram = px.histogram(df, x = 'Response Time', nbins = 20, labels = {'Response Time': 'Response Time (days)', 'count': 'Count'}, title = "Application Speed")
 st.plotly_chart(histogram)
 '''
 Hover over the graph with your mouse to see more information!
