0d57defc4c5b17e0c1fb390aadc0d03d363bd685
jareddingman <jareddingman@gmail.com>
2025-05-05 15:40:15 -0500

    Update Executive Summary.py

--- Dashboard/pages/Executive Summary.py
+++ Dashboard/pages/Executive Summary.py
@@ -1,9 +1,49 @@
 import streamlit as st
+import pandas as pd
+import openpyxl
+from datetime import datetime
+import re
+import requests
+import numpy as np
+import plotly.express as px
 
-st.title("Executive Summary :breifcase:")
+#be sure to also import the requirements.txt in your terminal
+
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(page_title='Semester Project')
+uploadedFile = st.file_uploader("Choose file here:", type = ["csv", "xlsx"])
+# -----------------------------------------------------------------------------
+# Declare some useful functions.
+
+@st.cache_data
+def load_original_data():
+    if uploadedFile == None:
+        url = 'https://raw.githubusercontent.com/jareddingman/semesterproject-dashboard/main/data/UNO%20Service%20Learning%20Data%20Sheet%20De-Identified%20Version(1).xlsx'
+        response = requests.get(url)
+        if response.status_code == 200:
+            return pd.read_excel(url, engine = "openpyxl")
+        else:
+            st.error("Failed to load data from GitHub.")
+            return None
+#-----------------------------------------------------------------------------
+df_initial = load_original_data()
+
+
+
+
+
+
+
+
+
+
+
+
+st.title("Executive Summary :briefcase:")
 ''
 ''
 st.write("This summary captures some key points that may be important to NCS HOPE. At the bottom of this page, there is are data recommendations that might be useful to NCS HOPE.")
+
 st.subheader("Recommendations")
 st.write("After Handling this data to create this app, I have a few suggestions for NCS HOPE:")
 ''
