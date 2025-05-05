4fba0c731bcafac97238642dee7c053cba18bc39
jareddingman <jareddingman@gmail.com>
2025-05-05 15:38:25 -0500

    Update Executive Summary.py

--- Dashboard/pages/Executive Summary.py
+++ Dashboard/pages/Executive Summary.py
@@ -13,8 +13,12 @@ Create a fixed-choice format for entering data.
 - Many of the recorded instances had to be thrown out or manually changed (not all spelling errors were fixed).
 - For example, this app makes any "Missing", "missing", "MIssing" into a blank space. By having a fixed-choice format, NCS HOPE would not have to rely on the robustness of this code.
 - This would also help with the 'Request Status' column which shows Approved/Pending/Denied AND dates
+'''
+'''
 Refine column descriptions.
 - More accurately describing each column and HOW data is entered would be extremely useful to those analyzing data.
+'''
+'''
 Separate monetary information into a separate file.
 - The file given to us had multiple instances of 'Remaining Balance' for the same Patient ID number. In these multiple instances, the "Amount" tab had varying expenses used. Making these transactions in a separate (probably accounting) document would help give more accurate answers in data analysis.
 '''
